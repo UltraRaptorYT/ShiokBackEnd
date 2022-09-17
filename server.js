714f2a41bf45f9c39d6d25d99d900c5e103cdfc0
UltraRaptorYT <62703912+UltraRaptorYT@users.noreply.github.com>
2022-09-18 02:05:54 +0800

    a

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const cors = require("cors");
+
+var app = express();
+
+app.use(cors());
+const SerpApi = require("google-search-results-nodejs");
+const search = new SerpApi.GoogleSearch(
+  "67dffa269b9484439b79ad78beb99613ac8bd9c64c4653072d5c7c020dbfe0cf"
+);
+
+const params = {
+  engine: "google",
+  q: "Coffee",
+};
+
+const callback = function (data) {
+  console.log(data["organic_results"]);
+};
+
+// Show result as JSON
+search.json(params, callback);
+app.get("/", (req, res) => {
+  return "Hi";
+});
+
+const PORT = 8081;
+app.listen(PORT, () => {
+  console.log(`Server started on port localhost:${PORT}`);
+});
